ebcaa96d71e79e8ae99f70d9badab42d504596e5
Timur <timk01@mail.ru>
2026-08-07 01:46:44 +0300

    cloud-storage: - добавлена группа тестов (пока 2 ручки - рега, логин; пока - не до конца), в качестве тест-клиента выбран DEA HTTP Client;

--- src/main/java/storage/cloud/cloudstorage/request/UserLoginRequest.java
+++ src/main/java/storage/cloud/cloudstorage/request/UserLoginRequest.java
@@ -5,11 +5,11 @@ import jakarta.validation.constraints.Size;
 
 public record UserLoginRequest(
         @NotBlank(message = "User name should not be null and must contain at least one non-whitespace character")
-        @Size(min = 2, max = 100, message = "User name must be between {min} and {max} characters")
+        @Size(min = 3, max = 100, message = "User name must be between {min} and {max} characters")
         String userName,
 
         @NotBlank(message = "User password should not be null and must contain at least one non-whitespace character")
-        @Size(min = 2, max = 100, message = "User password must be between {min} and {max} characters")
+        @Size(min = 3, max = 100, message = "User password must be between {min} and {max} characters")
         String password
 ) {
 }
