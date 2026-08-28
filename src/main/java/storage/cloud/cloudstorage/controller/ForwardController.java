4448636af3f48bf93ef8dc0ea3f8e25969bb6fcc
Timur <timk01@mail.ru>
2026-08-28 13:00:17 +0300

    cloud-storage: - сделан форфард со страницы /files на /index.html;

---
+++ src/main/java/storage/cloud/cloudstorage/controller/ForwardController.java
@@ -0,0 +1,13 @@
+package storage.cloud.cloudstorage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ForwardController {
+
+    @GetMapping({"/files", "/files/"})
+    public String forwardToIndexPage() {
+        return "forward:/index.html";
+    }
+}
